93af71caf1daa366cb94bde38ab2a0d7641b1f19
er-ri <724chen@gmail.com>
2023-12-19 10:33:42 +0900

    feat: delisted code exception

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='fscraper',
-    version='1.0.4',
+    version='1.0.5',
     description='Financial Data Web Scraper',
     long_description=long_description,
     long_description_content_type='text/markdown',
