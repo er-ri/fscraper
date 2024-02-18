c8dc1ca31c7e3f11275d28a4b55dc907d20af463
er-ri <724chen@gmail.com>
2024-02-18 11:16:42 +0900

    build(release): version to 1.0.9

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='fscraper',
-    version='1.0.8',
+    version='1.0.9',
     description='Financial Data Web Scraper',
     long_description=long_description,
     long_description_content_type='text/markdown',
--- tests.py
+++ tests.py
@@ -1,10 +1,13 @@
 import time
 import unittest
 import fscraper as fs
-from fscraper.exceptions import CodeNotFoundException, NoArticleFoundException
+from fscraper.exceptions import CodeNotFound
 
 
 class TestMethods(unittest.TestCase):
+
+    def tearDown(self):
+        time.sleep(1)
 
     def test_yf_normal(self):
         code = '7203.T'
@@ -19,29 +22,14 @@ class TestMethods(unittest.TestCase):
 
         yf = fs.YahooFinanceScraper(code=code)
 
-        with self.assertRaises(CodeNotFoundException):
+        with self.assertRaises(CodeNotFound):
             yf.get_stock_price2(start='2023-12-15')
 
     def test_reuter_news(self):
-        keyword = '7203.T'
-        rs = fs.ReutersScraper(keyword)
-
-        news = rs.get_news(keyword, 5)
-
+        keyword = 'トヨタ自動車'
+        news = fs.ReutersScraper.get_news(keyword, 5)
         self.assertEqual(len(news), 5)
 
-    def test_reuter_news_nofetched(self):
-        keyword = 'asdf.T'
-        rs = fs.ReutersScraper(keyword)
-
-        with self.assertRaises(NoArticleFoundException):
-            rs.get_news(keyword, 5)
-
-    def test_reuter_financial_report(self):
-        rs = fs.ReutersScraper('7203.T')
-        report = rs.get_financials()
-
-        self.assertEqual(len(report), 10)
 
     def test_minkabu_news_list(self):
         ms = fs.MinkabuScraper('7203.T')
@@ -50,6 +38,12 @@ class TestMethods(unittest.TestCase):
         news_list = ms.get_news_list(queries[:3])
 
         self.assertEqual(len(news_list), 3)
+
+    def test_yfscraper_report(self):
+        yf = fs.YahooFinanceScraper('7203.T')
+        df = yf.get_financials('cashflow', 'annual')
+        self.assertGreater(len(df), 0)
+
 
 if __name__ == '__main__':
     unittest.main()
